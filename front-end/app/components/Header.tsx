935bbedbe2cb831814bed3a9c225d04b7c372551
Aboubakar Bamba <aboubakarbamba718@gmail.com>
2026-05-22 12:18:33 +0200

    création du header et ajout de la font du projet

---
+++ front-end/app/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <div className="flex justify-between">
+      <div>
+        <h1>Logo de l&apos;entrprise</h1>
+      </div>
+      <div className="flex gap-10 items-center">
+        <div>
+          <Link href="/">Services</Link>
+        </div>
+        <div>
+          <Link href="/">Réalisations</Link>
+        </div>
+        <div>
+          <Link href="/">Expertise</Link>
+        </div>
+        <div className="bg-transparent rounded-xl px-7 p-1 border border-black">
+          <Link href="/">Contact</Link>
+        </div>
+        <div className="bg-[#5D3A1A] text-white  rounded-xl px-7 p-1">
+          <Link href="/">Devis gratuit</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
